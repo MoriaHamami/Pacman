4494c6f90fbabca8ba010d01cb49aa24c621c565
Moria Hamami <100145853+MoriaHamami@users.noreply.github.com>
2022-11-22 22:28:54 +0200

    Uploading pacman files

---
+++ utils.js
@@ -0,0 +1,73 @@
+'use strict'
+
+function renderBoard(mat, selector) {
+
+    var strHTML = '<table border="0"><tbody>'
+    for (var i = 0; i < mat.length; i++) {
+
+        strHTML += '<tr>'
+        for (var j = 0; j < mat[0].length; j++) {
+
+            const cell = mat[i][j]
+            const className = `cell cell-${i}-${j}`
+
+            strHTML += `<td class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
+
+// location is an object like this - { i: 2, j: 7 }
+function renderCell(location, value) {
+    // Select the elCell and set the value
+    const elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
+    elCell.innerHTML = value
+}
+
+function getEmptyRandCell() {
+    const emptyCells = []
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[0].length; j++) {
+            const currCell = gBoard[i][j]
+            if (currCell === EMPTY) emptyCells.push({ i, j })
+        }
+    }
+    if(emptyCells.length === 0) return -1
+    const randIdxForBall = getRandomInt(0, emptyCells.length)
+    return emptyCells[randIdxForBall]
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
+function hideElement(selector) {
+    const el = document.querySelector(selector)
+    el.classList.add('hidden')
+}
+
+function showElement(selector) {
+    const el = document.querySelector(selector)
+    el.classList.remove('hidden')
+}
+
